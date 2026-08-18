2f213aa0793162e6b8d3b482b9eac8bcf2720b09
KairoqX <aakashgamedev123@gmail.com>
2026-08-18 19:48:40 +0530

    fixed email print after error

--- Python/beginner-projects/email-id-genrator.py
+++ Python/beginner-projects/email-id-genrator.py
@@ -11,7 +11,7 @@ provid = input('''chose provider:
 
 # print(provid)
 text = name+surname
-provider = ["gamil.com","proton.me","outlook.com","yahoo.com"]
+provider = ["gmail.com","proton.me","outlook.com","yahoo.com"]
 
 num1 = str(random.randint(0,9))
 num2 = str(random.randint(0,9))
@@ -20,12 +20,18 @@ num = num1+num2+num3
 
 if provid == "0":
     providers = provider[0]
+    print("Your email is",text+num+"@"+providers)
 elif provid == "1":
     providers = provider[1]
+    print("Your email is",text+num+"@"+providers)
 elif provid == "2":
     providers = provider[2]
+    print("Your email is",text+num+"@"+providers)
 elif provid == "3":
     providers = provider[3]
+    print("Your email is",text+num+"@"+providers)
 else:
     print("Please enter a valid provider id")
-print("Your email is",text+num+"@"+providers)
+
+
+
