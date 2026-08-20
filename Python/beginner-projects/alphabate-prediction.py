767c167d3be9e85dd87d0246e8627f9f851b6106
KairoqX <aakashgamedev123@gmail.com>
2026-08-20 22:57:02 +0530

    alphabate prediction

---
+++ Python/beginner-projects/alphabate-prediction.py
@@ -0,0 +1,15 @@
+letter = input("Type the letter: ").lower()
+
+if letter == "a":
+    print("its the first letter next letter is b.")
+
+elif letter == "b":
+    print("a then b then c")
+elif letter == "c":
+    print("b then c then d")
+elif letter == "d":
+    print("c then d then e")
+elif letter == "e":
+    print("d then e then f")
+elif letter == "f":
+    print("e then f then g")
