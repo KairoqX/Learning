4155bc8d8f9d0a847f1f560c4b3ee577771152d7
KairoqX <aakashgamedev123@gmail.com>
2026-08-21 21:39:02 +0530

    practice

---
+++ Python/Level-4/practice-1.py
@@ -0,0 +1,20 @@
+fruits = []
+
+user1 = input("Enter a fruit name: ")
+fruits.append(user1)
+
+user2 = input("Enter a fruit name: ")
+fruits.append(user2)
+user3 = input("Enter a fruit name: ")
+fruits.append(user3)
+user4 = input("Enter a fruit name: ")
+fruits.append(user4)
+user5 = input("Enter a fruit name: ")
+fruits.append(user5)
+user6 = input("Enter a fruit name: ")
+fruits.append(user6)
+user7 = input("Enter a fruit name: ")
+fruits.append(user7)
+
+
+print(fruits)
