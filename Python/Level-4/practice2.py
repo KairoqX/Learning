b54ebc3947e3fb3fa677376b0b30c69fe090d746
KairoqX <aakashgamedev123@gmail.com>
2026-08-21 21:42:10 +0530

    practice

---
+++ Python/Level-4/practice2.py
@@ -0,0 +1,18 @@
+markes = []
+
+user1 = int(input("Enter marks: "))
+markes.append(user1)
+
+user2 = int(input("Enter marks: "))
+markes.append(user2)
+user3 = int(input("Enter marks: "))
+markes.append(user3)
+user4 = int(input("Enter marks: "))
+markes.append(user4)
+user5 = int(input("Enter marks: "))
+markes.append(user5)
+user6 = int(input("Enter marks: "))
+markes.append(user6)
+
+markes.sort()
+print(markes)
