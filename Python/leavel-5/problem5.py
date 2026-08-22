d93db8a6e4a849a0e7fef51f3eb891b92ae4c2cb
KairoqX <aakashgamedev123@gmail.com>
2026-08-22 23:25:47 +0530

    practice

--- Python/leavel-5/problem5.py
+++ Python/leavel-5/problem5.py
@@ -4,4 +4,16 @@ name = input("Enter friends name: ")
 
 lang = input("Enter language: ")
 d.update({name: lang})
+name = input("Enter friends name: ")
+
+lang = input("Enter language: ")
+d.update({name: lang})
+name = input("Enter friends name: ")
+
+lang = input("Enter language: ")
+d.update({name: lang})
+name = input("Enter friends name: ")
+
+lang = input("Enter language: ")
+d.update({name: lang})
 print(d)
